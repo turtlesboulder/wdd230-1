cbbb67997da79c5bbfae7ba209d1967754936d0d
turtlesboulder <lturtlesrock@gmail.com>
2022-04-20 12:19:30 -0600

    Updated the time format

--- navigation.js
+++ navigation.js
@@ -1,5 +1,13 @@
 function add_date(){
-    let current_date = Date()
+    let current_date = new Date()
     let p_element = document.getElementById("to_be_manipulated")
-    p_element.innerHTML = `The time is ${current_date}`
+    let month = current_date.getMonth()
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    month = months[month]
+    let year = current_date.getFullYear()
+    let day = current_date.getDate()
+    let hour = current_date.getHours()
+    let minutes = current_date.getMinutes()
+    let seconds = current_date.getSeconds()
+    p_element.innerHTML = `It is ${month} ${day}, ${year} at ${hour}:${minutes} and ${seconds} seconds.`
 }
