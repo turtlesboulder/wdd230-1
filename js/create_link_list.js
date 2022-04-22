4c21be6d5a284694f18560175212511aad1f526c
turtlesboulder <lturtlesrock@gmail.com>
2022-04-22 16:21:16 -0600

    cleaned up folders

--- js/create_link_list.js
+++ js/create_link_list.js
@@ -5,7 +5,7 @@ let week_upper_bound = 13;
 for (let week_num = week_lower_bound; week_num <= week_upper_bound; week_num++){
     let li_container = document.createElement("li");
     let link_container = document.createElement("a");
-    link_container.href = `week${week_num}/week${week_num}.html`;
+    link_container.href = `weeks/week${week_num}/week${week_num}.html`;
     link_container.innerHTML = `Week ${week_num}`;
     link_list.append(li_container);
     link_list.lastChild.append(link_container);
