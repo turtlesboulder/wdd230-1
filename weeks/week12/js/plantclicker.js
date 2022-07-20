9635c8512b1ec022fbaeff4fe1e9665d3daae1c9
turtlesboulder <lturtlesrock@gmail.com>
2022-07-19 23:19:59 -0600

    localStorage

--- weeks/week12/js/plantclicker.js
+++ weeks/week12/js/plantclicker.js
@@ -2,6 +2,18 @@ function doShears(){
     let shears = document.querySelector("#shears")
     shears.shears = 0
     shears.bananasNeeded = 1
+    let localShears = localStorage.getItem("shears")*1
+    if (localShears){
+        shears.shears = localShears
+        document.querySelector("#shears h3").textContent = `Shears: ${shears.shears}`
+        document.querySelector("#shears").style = "display: block;"
+    }
+    let localShearsBananasNeeded = localStorage.getItem("shearsBananasNeeded")*1
+    if (localShearsBananasNeeded){
+        shears.bananasNeeded = localShearsBananasNeeded
+        document.querySelector("#shears button").textContent = `Buy: ${shears.bananasNeeded} Bananas`
+    }
+    
     let runner = ()=>{
         let shears = document.querySelector("#shears")
         let plant = document.querySelector("#plant")
@@ -16,6 +28,8 @@ function doShears(){
             }else{
                 document.querySelector("#shears button").textContent = `Buy: ${shears.bananasNeeded} Bananas`
             }
+            localStorage.setItem("shears", shears.shears)
+            localStorage.setItem("shearsBananasNeeded", shears.bananasNeeded)
         }
         
     }
@@ -25,6 +39,19 @@ function doFertilizer(){
     let fertilizer = document.querySelector("#fertilizer")
     fertilizer.fertilizer = 0
     fertilizer.leavesNeeded = 200
+
+    let localFertilizer = localStorage.getItem("fertilizer")*1
+    if (localFertilizer){
+        fertilizer.fertilizer = localFertilizer
+        document.querySelector("#fertilizer h3").textContent = `Fertilizer: ${fertilizer.fertilizer}`
+        document.querySelector("#fertilizer").style = "display: block;"
+    }
+    let localFertilizerleavesNeeded = localStorage.getItem("fertilizerLeavesNeeded")*1
+    if (localFertilizerleavesNeeded){
+        fertilizer.leavesNeeded = localFertilizerleavesNeeded
+        document.querySelector("#fertilizer button").textContent = `Buy: ${fertilizer.leavesNeeded} Leaves`
+    }
+
     let runner = ()=>{
         let fertilizer = document.querySelector("#fertilizer")
         let plant = document.querySelector("#plant")
@@ -35,6 +62,8 @@ function doFertilizer(){
             fertilizer.leavesNeeded += 100
             document.querySelector("#fertilizer h3").textContent = `Fertilizer: ${fertilizer.fertilizer}`
             document.querySelector("#fertilizer button").textContent = `Buy: ${fertilizer.leavesNeeded} Leaves`
+            localStorage.setItem("fertilizer", fertilizer.fertilizer)
+            localStorage.setItem("fertilizerLeavesNeeded", fertilizer.leavesNeeded)
         }
         
     }
@@ -43,6 +72,18 @@ function doFertilizer(){
 
 function doPlant(){
     let plant = document.querySelector("#plant")
+
+    let localLeaves = localStorage.getItem("leaves")*1
+    let localBananas = localStorage.getItem("bananas")*1
+    if (localLeaves){
+        plant.leaves = localLeaves
+        document.querySelector("#leaves-display").textContent = `Leaves: ${plant.leaves}`
+    }
+    if (localBananas){
+        plant.bananas = localBananas
+        document.querySelector("#bananas-display").textContent = `Bananas: ${plant.bananas}`
+    }
+
     let runner = ()=>{
         let fertilizer = document.querySelector("#fertilizer")
         randomNum = 0
@@ -54,10 +95,12 @@ function doPlant(){
         }
         let shears = document.querySelector("#shears")
         plant.leaves += (shears.shears + 1)
+        localStorage.setItem("leaves", plant.leaves)
         if (randomNum > 0.99){
             plant.bananas += 1
             document.querySelector("#bananas-display").textContent = `Bananas: ${plant.bananas}`
             document.querySelector("#shears").style = "display: block;"
+            localStorage.setItem("bananas", plant.bananas)
         }
         document.querySelector("#leaves-display").textContent = `Leaves: ${plant.leaves}`
         if (plant.leaves >= 200){
